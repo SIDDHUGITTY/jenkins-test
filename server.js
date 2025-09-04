40ebac36ef0b1cc0ee867894d0718e209e3eb7ea
SIDDHUGITTY <142041319+SIDDHUGITTY@users.noreply.github.com>
2025-09-04 17:30:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Docker!");
+  res.send("Hello from Docker + Node.js!");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
